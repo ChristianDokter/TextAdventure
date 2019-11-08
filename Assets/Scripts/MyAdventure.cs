0cbde9eceb6b886eb30bf10ab7a467d5f76cc712
Christian Dokter <328370@student.mboutrecht.nl>
2019-11-08 11:23:48 +0100

    no message

--- Assets/Scripts/MyAdventure.cs
+++ Assets/Scripts/MyAdventure.cs
@@ -1,47 +1,80 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Security;
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
+using Random = UnityEngine.Random;
 
 public class MyAdventure : MonoBehaviour
 {
+
+    private enum States
+    {
+        Mainmenu,
+        Gamestart,
+        controls,
+        credits,
+    }
+
+    private States currentState = States.Mainmenu;
+
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello World!");
-        ShowMainMenu();
+        Mainmenu();
     }
 
-    void ShowMainMenu()
-    {
-        Terminal.ClearScreen();
-        Terminal.WriteLine("Welkom bij AstroWorld");
-        Terminal.WriteLine("Dit is gebaseerd op een waargebeurd verhaal");
-        Terminal.WriteLine("Typ start om te beginnen");
-    }
-
-    void OnUserInput(string input)
-    {
-        if (input == "start")
-        {
-            StartIntro();
-        }else if (input == "6969")
-        {
-            Terminal.WriteLine("Epic!");
-        }
-        else
-        {
-            Terminal.WriteLine("Verkeerde invoer");
-        }
-    }
-
-    void StartIntro()
-    {
-        Terminal.WriteLine("Het was een rustige dag in het RuimteSchip");
-    }
+    
+   
 
     // Update is called once per frame
-    void Update()
+    void OnUserInput(string input)
     {
+        switch (currentState)
+        {
+            case States.Mainmenu:
+                if (input == "Start")
+                {
+                    GameStart();
+                }
+                else if (input == "controls")
+                {
+                    controls();
+                }
+                else if (input == "credits")
+                {
+                    credits();
+                }
+                else
+                {
+                    Mainmenu();
+                }
+        }
+    }
+
+    void Mainmenu()
+    {
+        currentState = States.Mainmenu;
         
+    }
+
+    void GameStart()
+    {
+
+    }
+
+    void controls()
+    {
+
+    }
+
+    void credits()
+    {
+
     }
 }
